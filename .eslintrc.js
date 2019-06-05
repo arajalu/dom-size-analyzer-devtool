528d4aebab145630dcd4db0d01aae8d8ce337bee
Anand Rajalu <arajalu@gmail.com>
2019-06-05 19:24:48 +0530

    ran prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -78,5 +78,5 @@ module.exports = {
     'redux-saga/no-yield-in-race': 2,
     'redux-saga/yield-effects': 2,
     'require-yield': 0,
-  }
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,45 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: [
-    path.resolve('./src/components/index.jsx')
-  ],
+  entry: [path.resolve('./src/components/index.jsx')],
   output: {
     path: path.resolve('./build/'),
     filename: 'app.js',
-    publicPath: '.'
+    publicPath: '.',
   },
   module: {
     rules: [
       {
         test: /\.jsx/,
         include: path.resolve('./src/components/'),
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.s?css$/,
         use: [
-          {loader: 'style-loader'},
+          { loader: 'style-loader' },
           {
             loader: 'css-loader?importLoaders=1',
             query: {
               modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]'
-            }
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
           },
-          {loader: 'sass-loader'},
+          { loader: 'sass-loader' },
           {
             loader: 'postcss-loader',
             options: {
-              plugins: function () {
-                return [
-                  require('postcss-cssnext')
-                ];
-              }
-            }
-          }
-        ]
-      }
-    ]
+              plugins: () => [require('postcss-cssnext')],
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-    new CopyPlugin([
-      { from: './src/devtools/*', to: './' ,flatten:true},
-    ]),
+    new CopyPlugin([{ from: './src/devtools/*', to: './', flatten: true }]),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.scss', '.css']
+    extensions: ['.js', '.jsx', '.scss', '.css'],
   },
 };
