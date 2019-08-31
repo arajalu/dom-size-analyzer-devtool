c35f6cee05f1e62294a780a16c32ef55afcca28e
Anand Rajalu <arajalu@gmail.com>
2019-08-31 13:02:43 +0530

    fixed:Background and Contenscript are not getting injected into existing tabs after installation/update

--- src/devtools/backgroundScript.js
+++ src/devtools/backgroundScript.js
@@ -6,3 +6,31 @@ chrome.runtime.onConnect.addListener(devToolsConnection => {
     devToolsConnection.postMessage(request);
   });
 });
+
+/**
+ * injecting contentscript(s) for existing tabs
+ */
+chrome.manifest = chrome.app.getDetails();
+
+const injectIntoTab = tab => {
+  if (tab) {
+    const scripts = chrome.manifest.content_scripts[0].js;
+    const s = scripts.length;
+    for (let i = 0; i < s; i += 1) {
+      chrome.tabs.executeScript(tab.id, {
+        file: scripts[i],
+      });
+    }
+  }
+};
+chrome.windows.getCurrent(
+  {
+    populate: true,
+  },
+  window => {
+    for (let j = 0; j < window.tabs.length; j += 1) {
+      const currentTab = window.tabs[j];
+      injectIntoTab(currentTab);
+    }
+  },
+);
